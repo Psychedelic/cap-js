8cf90af534f30b54a3d40b30cb4508cd626a1def
Helder Oliveira <heldrida@gmail.com>
2021-10-15 15:10:06 +0100

    fix: 🐛 exports type definitions for typescript, explicitly via index.d.ts

---
+++ src/declarations/cap/index.d.ts
@@ -0,0 +1,25 @@
+export {
+  default as Root,
+  Event,
+  GetBucketResponse,
+  GetNextCanistersResponse,
+  GetTransactionResponse,
+  GetTransactionsArg,
+  GetTransactionsResponseBorrowed,
+  GetUserTransactionsArg,
+  IndefiniteEvent,
+  Operation,
+  WithIdArg,
+  WithWitnessArg,
+  Witness,
+} from "./root";
+
+
+export {
+  default as Router,
+  GetIndexCanistersResponse,
+  GetTokenContractRootBucketArg,
+  GetTokenContractRootBucketResponse,
+  GetUserRootBucketsArg,
+  GetUserRootBucketsResponse,
+} from "./router";
