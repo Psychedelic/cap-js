bf2d674b37a2c9329a97b51a9fd4e71ab40e4950
Helder Oliveira <heldrida@gmail.com>
2021-10-19 20:51:16 +0100

    refactor: 💡 mainly removing all function wrappers of original actor methods

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,11 @@ import {
   HttpAgent,
   HttpAgentOptions,
 } from "@dfinity/agent";
-import { Principal } from "@dfinity/principal";
 
+import _ROUTER_SERVICE from "./declarations/cap/router";
+import _ROOT_SERVICE from "./declarations/cap/root";
 import { routerFactory } from "./declarations/cap/router.did.js";
-import _ROUTER_SERVICE, {
-  GetTokenContractRootBucketResponse,
-  GetUserRootBucketsResponse,
-} from "./declarations/cap/router";
-
 import { rootFactory } from "./declarations/cap/root.did.js";
-import _ROOT_SERVICE, {
-  GetTransactionResponse,
-  GetTransactionsArg,
-  GetTransactionsResponseBorrowed,
-  GetUserTransactionsArg,
-} from "./declarations/cap/root";
 
 export {
   Root,
@@ -50,162 +40,35 @@ export const Hosts = {
 
 // TODO: Temporary used as a reference while refactoring
 export const Canisters = {
+  mainnet: "rrkah-fqaaa-aaaaa-aaaaq-cai",
   local: "rrkah-fqaaa-aaaaa-aaaaq-cai",
 }
 
-export const rootActor = async (
-  canisterId: Principal
-): Promise<ActorSubclass<_ROOT_SERVICE>> => {
-  // const agent = new HttpAgent({ host: "https://ic0.app", fetch });
-  const agent = new HttpAgent({
-    host: "http://localhost:8000",
-    fetch,
-  } as unknown as HttpAgentOptions);
-
-  if (process.env.NODE_ENV !== "production") {
-    agent.fetchRootKey().catch((err) => {
-      console.warn(
-        "Unable to fetch root key. Check to ensure that your local replica is running"
-      );
-      console.error(err);
-    });
-  }
-
-  return Actor.createActor(rootFactory, {
-    agent,
-    canisterId,
-  });
-};
+type IdlFactory = ({ IDL }: { IDL: any; }) => any;
 
 export interface ActorParams {
   host: string,
   canisterId: string,
+  idlFactory: IdlFactory,
 }
 
-export const routerActor = async (): Promise<
-  ActorSubclass<_ROUTER_SERVICE>
-> => {
-  // const agent = new HttpAgent({ host: "https://ic0.app", fetch });
-  const agent = new HttpAgent({
-    host: "http://localhost:8000",
-    fetch,
-  } as unknown as HttpAgentOptions);
+export class CapBase <T>{
+  public actor: ActorSubclass<T>;
 
-  if (process.env.NODE_ENV !== "production") {
-    agent.fetchRootKey().catch((err) => {
-      console.warn(
-        "Unable to fetch root key. Check to ensure that your local replica is running"
-      );
-      console.error(err);
-    });
-  }
-
-  return Actor.createActor(routerFactory, {
-    agent,
-    canisterId: "rrkah-fqaaa-aaaaa-aaaaq-cai",
-  });
-};
-
-export const cap = {
-  /**
-   * Router Canister
-   */
-  async get_token_contract_root_bucket({
-    tokenId,
-    witness,
-  }: {
-    tokenId: Principal;
-    witness: boolean;
-  }): Promise<GetTokenContractRootBucketResponse> {
-    return (await routerActor()).get_token_contract_root_bucket({
-      canister: tokenId,
-      witness,
-    });
-  },
-  async get_user_root_buckets({
-    user,
-    witness,
-  }: {
-    user: string,
-    witness: boolean,
-  }): Promise<GetUserRootBucketsResponse> {
-    return (await routerActor()).get_user_root_buckets({
-      user: Principal.fromText(user),
-      witness,
-    });
-  },
-
-  /**
-   * Root Canister
-   */
-  async get_transaction(
-    tokenId: Principal,
-    txnId: bigint,
-    witness: boolean
-  ): Promise<GetTransactionResponse> {
-    return (await rootActor(tokenId)).get_transaction({
-      id: txnId,
-      witness,
-    });
-  },
-  async get_transactions({
-    tokenId,
-    witness,
-    page,
-  }: {
-    tokenId: Principal;
-    witness: boolean;
-    page?: number;
-  }): Promise<GetTransactionsResponseBorrowed> {
-    return (await rootActor(tokenId)).get_transactions({
-      page: page ? [page] : [],
-      witness,
-    } as GetTransactionsArg);
-  },
-  async get_user_transactions({
-    tokenId,
-    userId,
-    page = 0,
-    witness,
-  }: {
-    tokenId: Principal;
-    userId: Principal;
-    page?: number;
-    witness: boolean;
-  }): Promise<GetTransactionsResponseBorrowed> {
-    return (await rootActor(tokenId)).get_user_transactions(
-      {
-        page: page ? [page] : [],
-        user: userId,
-        witness,
-      } as GetUserTransactionsArg
-    );
-  },
-};
-
-export class Cap {
   constructor(
-    private host: string,
-    private canisterId: string,
-    public routerActor: ActorSubclass<_ROUTER_SERVICE>,
-    public rootActor: ActorSubclass<_ROOT_SERVICE>,
+    actor: ActorSubclass<T>,
   ) {
-    console.log(
-      '[debug] placeholders to skip linting warnings on build',
-      this.host,
-      this.canisterId,
-      typeof this.rootActor,
-    )
+    this.actor = actor;
   }
 
-  static async createActor<T>({
+  private static async createActor<T>({
     host,
     canisterId,
     idlFactory,
   }: {
     host: string,
     canisterId: string,
-    idlFactory: ({ IDL }: { IDL: any; }) => any,
+    idlFactory: IdlFactory,
   }): Promise<ActorSubclass<T>> {
     const agent = new HttpAgent({
       host,
@@ -227,67 +90,59 @@ export class Cap {
     });
   }
 
-  static init ({
+  public static inititalise <T>({
     host,
     canisterId,
+    idlFactory,
   }: ActorParams) {
     return (async () => {
-      const routerActor = await Cap.createActor<_ROUTER_SERVICE>({
+      const actor = await CapBase.createActor<T>({
         host,
         canisterId,
-        idlFactory: routerFactory,
+        idlFactory,
       });
 
-      const rootActor = await Cap.createActor<_ROOT_SERVICE>({
-        host,
-        canisterId,
-        idlFactory: rootFactory,
-      });
-
-      const cap = new Cap(
-        host,
-        canisterId,
-        routerActor,
-        rootActor,
+      const cap = new CapBase<T>(
+        actor,
       );
 
       return cap;
     })();
   }
+}
 
-  async get_user_root_buckets({
-    user,
-    witness,
+export class CapRouter extends CapBase <_ROUTER_SERVICE>{
+  public static init({
+    host = Hosts.mainnet,
+    canisterId = Canisters.mainnet,
   }: {
-    user: Principal,
-    witness: boolean,
-  }): Promise<GetUserRootBucketsResponse | void> {
-    if (!this.routerActor) {
-      console.warn('Oops! The router actor was not instantiated yet');
-      return;
-    };
-
-    return this.routerActor.get_user_root_buckets({
-      user,
-      witness,
-    });
+    host?: string,
+    canisterId: string,
+  }) {
+    return (async () => {
+      return await CapBase.inititalise<_ROUTER_SERVICE>({
+        host,
+        canisterId,
+        idlFactory: routerFactory,
+      })
+    })();
   }
+}
 
-  async get_transactions({
-    witness,
-    page,
+export class CapRoot extends CapBase <_ROOT_SERVICE>{
+  public static init({
+    host = Hosts.mainnet,
+    canisterId,
   }: {
-    witness: boolean;
-    page?: number;
-  }): Promise<GetTransactionsResponseBorrowed | void> {
-    if (!this.rootActor) {
-      console.warn('Oops! The router actor was not instantiated yet');
-      return;
-    };
-
-    return this.rootActor.get_transactions({
-      page: page ? [page] : [],
-      witness,
-    } as GetTransactionsArg);
+    host?: string,
+    canisterId: string,
+  }) {
+    return (async () => {
+      return await CapBase.inititalise<_ROOT_SERVICE>({
+        host,
+        canisterId,
+        idlFactory: rootFactory,
+      })
+    })();
   }
 }
